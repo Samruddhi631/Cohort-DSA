8c39a525328fe939c584c5595fe41dffddbfe017
samruddhi <samruddhisuryawanshi139@gmail.com>
2025-07-02 22:23:22 +0530

    10th day of DSA

---
+++ mathProblems.js
@@ -0,0 +1,28 @@
+// print prime number till n range 
+
+// let n=30;
+// let arr=new Array(n+1).fill(true)
+// for(let i=2;i<=Math.floor(Math.sqrt(n));i++){
+//     if(arr[i]===true){
+//         for(let j=i*i;j<=n;j+=i){
+//             arr[j]=false
+//         }
+//     }
+// }
+// for(let i=2;i<=arr.length-1;i++){
+//     if(arr[i]===true){
+//         process.stdout.write(i+" ")
+//     }
+// }
+
+// find the sqrt of n 
+
+// let n=15;
+// function sqrt(n){
+//     let i=1
+//     for(;i*i<=n;i++){
+//         if(i*i===n) return i
+// }
+//     return i-1
+// }
+// console.log(sqr)
--- recursion.js
+++ recursion.js
@@ -52,9 +52,6 @@
 
 // fibonnaci series in iterative way 
 
-let n=12;
-let first=0,second=1;
-process.stdout.write(first+" "+second+" ")
 // for(let i=1;i<=n-2;i++){
 //     if (n==0||n==1) break;
 //     let third=first+second;
@@ -62,10 +59,117 @@ process.stdout.write(first+" "+second+" ")
 //     first=second;
 //     second=third
 // }
-function fibonnaci(n,first,second){
-    if(n==0) return
-    let third=first+second
-    process.stdout.write(third+" ")
-    fibonnaci(n-1,second,third)
-}
-fibonnaci(n-2,first,second)
+
+// fibonacci series using recursion
+
+    // let n=10
+    // if (n <= 0) {
+    //     console.log("Invalid input")
+    //     return;
+    // }
+    // let first = 0, second = 1;
+    // if (n === 1) {
+    //     process.stdout.write(first + " ");
+    //     return;
+    // }
+    // process.stdout.write(first + " " + second + " ");
+    // function fibonacci(count, a, b) {
+    //     if (count <= 2) return; // already printed 2 numbers
+    //     let next = a + b;
+    //     process.stdout.write(next + " ");
+    //     fibonacci(count - 1, b, next);
+    // }
+    // fibonacci(n, first, second);
+
+// sum of fibnacci series
+
+// function fibo(n){
+//     if(n==0) return 0;
+//     if (n==1) return 1;
+//     return fibo(n-1)+fibo(n-2)
+// }
+// let n=10
+// console.log(fibo(n))
+
+// sum of digits of number
+
+// function sumOfDigits(n){
+//     if(n<10) return n
+//     // let rem=n%10
+//     return (n%10)+sumOfDigits(Math.floor(n/10))
+// }
+// console.log(sumOfDigits(34298))
+
+// reverse the number
+
+// function reverseDigits(n) {
+
+//     let numStr = String(n);
+//   let sign = '';
+//   if (numStr.startsWith('-')) {
+//     sign = '-';
+//     numStr = numStr.substring(1); // Remove the '-' for reversal
+//   }
+//    function revNum(n,rev){
+//     if (n===0) return rev
+//     return revNum(Math.floor(n/10),rev*10+(n%10))
+// }
+//   return sign+revNum(numStr,0)
+// }
+// console.log(reverseDigits(-134))
+
+// find the gcd
+
+// 20=1,2,4,5,10,20
+// 32=1,2,4,8,16,32
+// let fact
+// let a=20,b=32;
+// for(let i=(Math.min(a,b));i>=1;i--){
+//     if (a%i==0&&b%i==0){
+//        console.log(i)
+//    break;
+//     }
+// }
+
+
+// function gcd(a,b){
+//     if(a===b) return a;
+//     if(a>b) return gcd(a-b,b);
+//     return gcd(a,b-a)
+// }
+
+// let a=20,b=32;
+// console.log(gcd(a,b))
+
+// function gcd(a,b){
+//     if(b==0) return a
+//     return gcd(b,a%b);
+// }
+
+// find the power of n to p 
+
+// function power(n, p) {
+//     if (p === 0) return 1;               
+//     if (p < 0) return 1 / power(n, -p);  
+
+//     return n * power(n, p - 1);        
+// }
+// console.log(power(2.1, 3).toFixed(5));       
+// console.log(power(-2.0, 5).toFixed(5));
+
+// more optimized way 
+
+//  function power(n, p) {
+//      if (p === 0) return 1;
+//     if (p < 0) return 1 / power(n, -p);
+
+//     let half = power(n, Math.floor(p / 2));
+
+//     if (p % 2 === 0) {
+//         return half * half;
+//     } else {
+//         return n * half * half;
+//     }
+// }
+
+// console.log(power(n,p).toFixed(5))
