a97da49438dbe2dcdd77580047338ccaf48b285f
samruddhi <samruddhisuryawanshi139@gmail.com>
2025-07-06 12:46:27 +0530

    12th day of DSA

--- ArrayProblems.js
+++ ArrayProblems.js
@@ -360,3 +360,27 @@
 // }
 // console.log(j)
 
+// Duplicate zeroes:[1,0,2,3,0,4,5]->[1,0,0,2,3,0,0]
+let arr=[1,0,2,3,0,4,5];
+let zeros=0;
+for(let i=0;i<=arr.length-1;i++){
+    if(arr[i]==0){
+        zeros++
+    }
+}
+i=arr.length-1;
+j=(arr.length-1)+zeros;
+while(i>=j){
+    if(j<arr.length){
+        arr[j]=arr[i]
+    }
+    j--
+    if(arr[i]==0){
+        if(j<arr.length){
+            arr[j]=arr[i]
+        }
+        j--
+    }
+    i--
+}
+console.log(arr)
